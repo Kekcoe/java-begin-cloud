cc63b32e063f5b552704689cf4472568b863193d
kekco <kekcoe@gmail.com>
2024-05-28 10:14:41 +0300

    Добавлен Eureka клиент

---
+++ eureka-client/src/main/java/ru/kekco/micro/demo/eurekaclient/controller/TestController.java
@@ -0,0 +1,13 @@
+package ru.kekco.micro.demo.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+   public String test() {
+        return "Test";
+    }
+}
