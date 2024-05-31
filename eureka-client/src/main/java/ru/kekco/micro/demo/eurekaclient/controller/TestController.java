7784ae746586d601b0a22648a94ab4d9cbd48dcc
kekco <kekcoe@gmail.com>
2024-05-31 10:09:58 +0300

    Добавлен ручной роутинг сервиса

--- eureka-client/src/main/java/ru/kekco/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/kekco/micro/demo/eurekaclient/controller/TestController.java
@@ -1,12 +1,13 @@
 package ru.kekco.micro.demo.eurekaclient.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test")
    public String test() {
         return "Test";
     }
