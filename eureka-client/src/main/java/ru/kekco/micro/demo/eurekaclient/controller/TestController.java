1f3faba7db223ed8a4c81235fb382e398760c12c
kekco <kekcoe@gmail.com>
2024-06-02 19:01:37 +0300

    Протестирована работа API gateway с помощью создания разных инстансов микросервиса

--- eureka-client/src/main/java/ru/kekco/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/kekco/micro/demo/eurekaclient/controller/TestController.java
@@ -1,4 +1,5 @@
 package ru.kekco.micro.demo.eurekaclient.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
    public String test() {
-        return "Test";
+        return id;
     }
 }
